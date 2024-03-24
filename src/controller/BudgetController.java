5042770e27c2796ff311eb5a345848d34334f346
Kevin-Arackan <kevinara365@gmail.com>
2024-03-23 22:48:35 -0400

    Budget Controller Work

--- src/controller/BudgetController.java
+++ src/controller/BudgetController.java
@@ -2,7 +2,22 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.Scene;
 
 
 public class BudgetController {
+
+    @FXML
+    Text budgetLabel;
+
+    @FXML
+    Text expensesLabel;
+
+    @FXML
+    PieChart pieChart;
+    public void start() {
+    }
 }
