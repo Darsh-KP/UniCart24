b72012f29f09fbfd4920fd690c2c457a1642c191
Kevin-Arackan <kevinara365@gmail.com>
2024-03-24 10:03:59 -0400

    BudgetController

--- src/controller/BudgetController.java
+++ src/controller/BudgetController.java
@@ -1,26 +1,41 @@
 package controller;
 
+import database.Storage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import model.Product;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class BudgetController {
 
-    @FXML
-    Text budgetLabel;
+    @FXML private Text budgetLabel;
 
-    @FXML
-    Text expensesLabel;
+    @FXML private Text expensesLabel;
 
-    @FXML
-    PieChart pieChart;
+    @FXML private PieChart pieChart;
     public void start() {
-        
+        List<Product> products = Storage.getTopDiscountProducts();
+        int column = 0;
+        try {
+            for (Product product : products) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/view/ProductCard.fxml"));
+
+                AnchorPane anchorPane = fxmlLoader.load();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error!");
+        }
     }
 
 
